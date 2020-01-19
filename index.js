2f09ceb6ac33ab639e90ef914bc67732b09c07af
Brittany Javalera <brittjavalera@gmail.com>
2020-01-19 14:33:00 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+
+function submitData(name, email){
+    let configObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: "Steve", 
+            email: "steve@steve.com"})
+      };
+
+    return fetch("http://localhost:3000/users", configObject)
+    .then(function(response) {
+        return response.json();
+      })
+    .then(function(object) {
+        document.body.innerHTML = object[ "id" ]
+      })
+      .catch(function(error){
+          alert("A Curse!");
+          document.body.innerHTML = (error.message);
+      })
+    
+}
