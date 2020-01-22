3b1b65b76ce13f1d10034c0c08333aefa1bd7b9b
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-01-21 22:31:38 -0800

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 // Add your code here
 
-// configurationObject can be given certain properties to change fetch's behavior
 // let formData = {
 //   dogName: "Byron",
 //   dogBreed: "Poodle"
 // }
- 
+
+// configurationObject has 3 main components - method (http verb), headers (metadata), body (data from inputs field, must be string)
 // let configObj = {
 //   method: "POST",
 //   headers: {
 //     "Content-Type": "application/json",
 //     "Accept": "application/json"
 //   },
-//   body: JSON.stringify(formData)
+//   body: JSON.stringify(formData)   // converts Object to string
 // }
 
 // fetch("http://localhost:3000/dogs", configObj)
+
+// represents what server destination sends back to us, .json method converts body of response from JSON to vanilla JS object
 //   .then(function(response) {
 //     return response.json()
 //   })
+
+// argument is whatever was returned from response.json()
 //   .then(function(object) {
 //     console.log(object)
 //   })
+
+// will run if there is an error or unexpected results
 //   .catch(function(error) {
 //     alert("Bad things! Ragnarők!")
 //     console.log(error.message)
