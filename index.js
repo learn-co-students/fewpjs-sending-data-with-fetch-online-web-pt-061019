341e500f01ef2529e14d6037bd4ba2bb2d593c52
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-01-21 22:04:32 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Add your code here
+
+// configurationObject can be given certain properties to change fetch's behavior
+// let formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle"
+// }
+ 
+// let configObj = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify(formData)
+// }
+
+// fetch("http://localhost:3000/dogs", configObj)
+//   .then(function(response) {
+//     return response.json()
+//   })
+//   .then(function(object) {
+//     console.log(object)
+//   })
+//   .catch(function(error) {
+//     alert("Bad things! Ragnarők!")
+//     console.log(error.message)
+//   })
+
+
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email
+  }
+  
+  let obj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch("http://localhost:3000/users", obj)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(object) {
+      console.log(object)
+      document.body.innerHTML += object.id
+    })
+    .catch(function(error) {
+      console.log(error.message)
+      document.body.innerHTML = error.message
+    })
+}
